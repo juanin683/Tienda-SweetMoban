20b95be36d94c32b05142ea3bc2e0dd26d7943ae
juana espinoza <juana.espinoza543@gmail.com>
2023-02-20 13:37:00 -0300

    agregue carpeta JS

---
+++ js/carrito.js
@@ -0,0 +1,51 @@
+
+let user = Number(prompt('Bienvenido! Selecciona el descuento (25%,50%,60% o 75%)'));
+
+let totalapagar = 0;
+let precio = 0;
+let descuento1 = 25;
+let descuento2 = 50;
+let descuento3 = 60;
+let descuento4 = 75;
+
+
+
+
+function agregarpreciocarrito(precio){
+    return totalapagar += precio;
+}
+
+totalapagar = agregarpreciocarrito(3500);
+totalapagar = agregarpreciocarrito(3000);
+totalapagar = agregarpreciocarrito(2800);
+totalapagar = agregarpreciocarrito(2500);
+
+
+
+
+
+
+
+    if(user==descuento1){
+        totalapagar = totalapagar - (100*0.25)
+        console.log("Precio final con descuento es de: " + totalapagar);
+        
+    } else if (user == descuento2 ){
+        totalapagar = totalapagar - (100*0.50);
+        console.log("Precio final con descuento es de: " + totalapagar);
+        
+    } else if (user == descuento3 ){
+        totalapagar = totalapagar - (100*0.60);
+        console.log("Precio final con descuento es de: " + totalapagar);
+    } else if (user == descuento4 ){
+    totalapagar = totalapagar - (100*0.75);
+    console.log("Precio final con descuento es de: " + totalapagar);
+    
+    } else {
+    alert('Por favor selecciona una opcion');
+    };
+
+
+do {
+    alert("Vuelve a intentarlo")
+} while (descuento1 && descuento2 && descuento3 &&descuento4 > 100);
