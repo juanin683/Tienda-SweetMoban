76472a987a20fc1274f92d4240a40a72f666fe6f
juana espinoza <juana.espinoza543@gmail.com>
2023-02-22 19:06:27 -0300

    cambios en JS

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,15 +1,19 @@
+let nombre;
+
+do{
+    nombre= prompt("Ingrese metodo de pago");
+    console.log(nombre);
+}while(nombre);
+
+
 
 let user = Number(prompt('Bienvenido! Selecciona el descuento (25%,50%,60% o 75%)'));
-
 let totalapagar = 0;
 let precio = 0;
 let descuento1 = 25;
 let descuento2 = 50;
 let descuento3 = 60;
 let descuento4 = 75;
-
-
-
 
 function agregarpreciocarrito(precio){
     return totalapagar += precio;
@@ -20,8 +24,9 @@ totalapagar = agregarpreciocarrito(3000);
 totalapagar = agregarpreciocarrito(2800);
 totalapagar = agregarpreciocarrito(2500);
 
+let calcularDescuentos = function(){
 if(user==descuento1){
-    totalapagar = totalapagar - (100*0.25)
+    totalapagar = totalapagar - (100*0.25);
     console.log("Precio final con descuento es de: " + totalapagar);
     
 } else if (user == descuento2 ){
@@ -36,6 +41,20 @@ totalapagar = totalapagar - (100*0.75);
 console.log("Precio final con descuento es de: " + totalapagar);
 
 } else {
-alert('Por favor selecciona una opcion');
+    alert("Por favor selecciona una opcion")
 };
+
+}
+calcularDescuentos();
+
+
+
+
+
+
+
+
+
+
+
 
